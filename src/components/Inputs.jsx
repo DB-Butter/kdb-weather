61722dfa695682640442977585928d762661c0e9
DB-Butter <devbabs68@gmail.com>
2022-10-26 21:44:47 -0500

    edit search placehlder

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -35,7 +35,7 @@ function Inputs({setQuery, units, setUnits}) {
     <div className='flex flex-row justify-center my-6'>
 
         <div className='flex flex-row w-3/4 items-center space-x-4'>
-            <input value={city} onChange={(e) => setCity(e.currentTarget.value)} type="text" placeholder="search..." className='text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize'></input>
+            <input value={city} onChange={(e) => setCity(e.currentTarget.value)} type="text" placeholder="search by city name..." className='text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize'></input>
             <UilSearch onClick={handleSearchClick} size={25} className="text-white cursor-pointer transition ease-out hover:scale-125"/>
             <UilLocationPoint onClick={handleLocationClick} size={25} className="text-white cursor-pointer transition ease-out hover:scale-125"/>
         </div>
